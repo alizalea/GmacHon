7a74332e2c55a064acdf6cf80e1cc9b87d66b206
lea <lea.tvito@gmail.com>
2016-02-20 23:41:33 +0200

    add transaction

--- JS/Model/TransactionModel.js
+++ JS/Model/TransactionModel.js
@@ -1,5 +1,13 @@
 ﻿var GMach = {
 
+    transaction_type: {
+        LOAN: "loan",//הלוואה
+        RETURN_LOAN: "return_loan",//החזרת הלוואה
+        DEPOSIT: "deposit",//הפקדה
+        RETURN_DEPOSIT: "retur_deposit",//משיכת הפקדה
+        DONATION: "donation"//תרומה
+    },
+
     Transaction: function (transaction_type, contact, amount, transaction_date) {
         this.transaction_type = transaction_type;
         this.contact = contact;
@@ -7,8 +15,17 @@
         this.transaction_date = transaction_date;
 
     },
+
+    Transaction_Return: function (transaction_type, contact, amount, transaction_date, return_date, returned, return_amount) {
+        Gmach.Transaction.call(transaction_type, contact, amount, transaction_date);
+        this.return_date = return_date;
+        this.returned = returned;
+        this.return_amount = return_amount;
+    },
+
+
     GetAllTransactions: function () {
-        var Transactions = [new GMach.Transaction(1, "aa", 20, 21 / 2 / 2012),
+        var Transactions = [new GMach.Transaction(gm.transaction_type.Loan, "aa", 20, 21 / 2 / 2012),
                                     ,
         ];
 
