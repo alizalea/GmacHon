bceb89281c0699e6520563a4145202015d6f8fe8
lea <lea.tvito@gmail.com>
2016-02-20 23:20:21 +0200

    add transaction

---
+++ JS/Model/TransactionModel.js
@@ -0,0 +1,19 @@
+﻿var GMach = {
+
+    Transaction: function (transaction_type, contact, amount, transaction_date) {
+        this.transaction_type = transaction_type;
+        this.contact = contact;
+        this.amount = amount;
+        this.transaction_date = transaction_date;
+
+    },
+    GetAllTransactions: function () {
+        var Transactions = [new GMach.Transaction(1, "aa", 20, 21 / 2 / 2012),
+                                    ,
+        ];
+
+        return Contacts;
+
+    }
+
+};
