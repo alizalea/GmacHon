34761ab4d8ba94697516a4b25a7e7bf19c20644d
alizalea <lea.tvito@gmail.com>
2016-02-23 00:47:01 +0200

    editor

--- JS/UI/contactui.js
+++ JS/UI/contactui.js
@@ -1,11 +1,47 @@
 ﻿var allContacts = new GMach.GetAllContacts();
+var editor;
 $(document).ready(function () {
     $.get('Header.html', function (data) {
         $('#header').html(data);
 
     });
-    $('#contacts').DataTable({
+   
+    editor = new $.fn.dataTable.Editor( {
+        ajax: "../php/todo.php",
+        table: "#contacts",
+    fields: [ {
+        label: "שם משפחה:",
+        name:  "last_name"
+    },
+      {
+        label: "שם פרטי:",
+        name: "first_name",
+      },
+      
+      {
+          label: "תעודת זהות:",
+          name: "IdNumber",
+      },
+      {
+          label: "טלפון:",
+          name: "phoneNumber",
+      },
+   {
+       label: "טלפון סלולרי:",
+       name: "mobileNumber",
+   },
+ {
+     label: "הערות:",
+     name: "remarks",
+ }
 
+]
+
+} );
+    $('#contacts').DataTable({
+    dom: "Bfrtip",
+
+    ajax: "../php/todo.php",
         data: allContacts,
 
         columns: [
@@ -16,6 +52,17 @@ $(document).ready(function () {
              { "data": "mobileNumber" },
              { "data": "remarks" }
         ],
+        select: true,
+
+        buttons: [
+
+           { extend: "create", editor: editor },
+
+            { extend: "edit",   editor: editor },
+
+{ extend: "remove", editor: editor }
+
+              ],
         "language": {
 
             "lengthMenu": "מציג _MENU_ שורות לעמוד",
