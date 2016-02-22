85cdeab61176f4e06e8da8de47da5b044a4edade
alizalea <lea.tvito@gmail.com>
2016-02-22 08:09:36 +0200

    רכ

---
+++ JS/UI/TransactionUI.js
@@ -0,0 +1,46 @@
+﻿
+var allTransactions = new GMach.tr.GetAllTransactions();
+$(document).ready(function () {
+    $.get('Header.html', function (data) {
+        $('#header').html(data);
+
+    });
+    $('#transactions').DataTable({
+
+        data: allTransactions,
+
+        columns: [
+              { "data": "transaction_type" },
+             { "data": "contact" },
+            { "data": "amount" },
+             { "data": "transaction_date" },
+
+        ],
+        "language": {
+
+            "lengthMenu": "מציג _MENU_ שורות לעמוד",
+
+            "zeroRecords": "לא נמצאו תנועות",
+
+            "info": "מספר דף _PAGE_ מתוך _PAGES_",
+
+            "infoEmpty": "אין פריטים לתצוגה",
+
+            "infoFiltered": "(filtered from _MAX_ total records)",
+            "search": "חיפוש",
+
+            "paginate": {
+
+                "first": "עמוד ראשון",
+
+                "last": "עמוד אחרון",
+
+                "next": "הבא",
+
+                "previous": "הקודם"
+
+            },
+        }
+    });
+
+});
