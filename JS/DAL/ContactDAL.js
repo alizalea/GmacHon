0ad051eef0e5160b2c6591592658a2875de86ebd
lea <lea.tvito@gmail.com>
2016-02-24 23:09:44 +0200

    add contact dal

---
+++ JS/DAL/ContactDAL.js
@@ -0,0 +1,15 @@
+﻿var GMach = GMach || {};
+GMach.DAL = GMach.DAL || {};
+GMach.DAL.Contact = GMach.DAL.Contact || {};
+
+GMach.DAL.Contact.GetAllContacts = function () {
+    var Contacts = [new GMach.Model.Contact("משה", "כהן", "111111111", "02-1111111", "054-8402125", "", "תורם חבל על הזמן"),
+                                 new GMach.Model.Contact("יוסי", "לוי", "222222222", "024444444", "085-9999999", "", "חיחחייח"),
+                                 new GMach.Model.Contact("יוסי", "כהן", "33333333", "024444444", "085-4444444", "", "חיחחייח"),
+                                 new GMach.Model.Contact("יוסי", "לוי", "44444444", "024444444", "085-5555555", "", "חיחחייח"),
+                                 new GMach.Model.Contact("חיים", "טויו", "222222222", "024444444", "085-9999999", "", "חיחחייח"),
+                                 new GMach.Model.Contact("אריאל", "פכטר", "222222222", "024444444", "085-9999999", "", "חיחחייח")];
+
+    return Contacts;
+
+}
--- JS/Model/ContactModel.js
+++ JS/Model/ContactModel.js
@@ -14,14 +14,8 @@ GMach.Model.Contact = function (first_name, last_name, IdNumber, phoneNumber, mo
 },
 
 GMach.Model.Contact.GetAllContacts = function () {
-    var Contacts = [new GMach.Model.Contact("משה", "כהן", "111111111", "02-1111111", "054-8402125", "", "תורם חבל על הזמן"),
-                                 new GMach.Model.Contact("יוסי", "לוי", "222222222", "024444444", "085-9999999", "", "חיחחייח"),
-                                 new GMach.Model.Contact("יוסי", "כהן", "33333333", "024444444", "085-4444444", "", "חיחחייח"),
-                                 new GMach.Model.Contact("יוסי", "לוי", "44444444", "024444444", "085-5555555", "", "חיחחייח"),
-                                 new GMach.Model.Contact("חיים", "טויו", "222222222", "024444444", "085-9999999", "", "חיחחייח"),
-                                 new GMach.Model.Contact("אריאל", "פכטר", "222222222", "024444444", "085-9999999", "", "חיחחייח")];
-
-    return Contacts;
+   
+    return GMach.DAL.Contact.GetAllContacts();
 
 }
 
