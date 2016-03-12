d76920812bef68d89f3df41205d0e4ce46181b9b
lea <lea.tvito@gmail.com>
2016-03-12 23:53:25 +0200

    numberWithCommas

--- JS/UI/OneGmachUI.js
+++ JS/UI/OneGmachUI.js
@@ -2,7 +2,11 @@
  
     var oneGmach = new GMach.Model.OneGmach();
    
-    $("#inputMoney").text(oneGmach.GetInputMoney());
-    $("#outputMoney").text(oneGmach.GetOutputMoney());
-    $("#diffMoney").text(oneGmach.GetDiffMoney());
+    $("#inputMoney").text(numberWithCommas(oneGmach.GetInputMoney()));
+    $("#outputMoney").text(numberWithCommas(oneGmach.GetOutputMoney()));
+    $("#diffMoney").text(numberWithCommas(oneGmach.GetDiffMoney()));
 });
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
