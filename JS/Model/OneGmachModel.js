cb914accd27ffed5d09d593fe5826490efe7fddc
lea <lea.tvito@gmail.com>
2016-03-12 22:26:06 +0200

    canges

--- JS/Model/OneGmachModel.js
+++ JS/Model/OneGmachModel.js
@@ -2,9 +2,12 @@
 GMach.Model = GMach.Model || {};
 GMach.Model.OneGmach = GMach.Model.OneGmach || {};
 
+var inputMoney, outputMoney, diffMoney;
+
 
 GMach.Model.OneGmach = function () {
-  
+
+    calcDiffMoney();
 },
 
 GMach.Model.OneGmach.prototype.Contacts = function () {
@@ -15,8 +18,27 @@ GMach.Model.OneGmach.prototype.Transactions = function () {
     return new GMach.Model.Transaction.GetAllTransactions();
 };
 
+function calcDiffMoney() {
+
+    inputMoney = 3008;
+    outputMoney = 2000;
+    diffMoney = inputMoney - outputMoney;
+}
+
 GMach.Model.OneGmach.prototype.GetInputMoney = function () {
 
-    return 0;
+    return inputMoney;
+
+}
+
+GMach.Model.OneGmach.prototype.GetOutputMoney = function () {
+
+    return outputMoney;
+
+}
+
+GMach.Model.OneGmach.prototype.GetDiffMoney = function () {
+
+    return diffMoney;
 
 }
---
+++ JS/UI/OneGmachUI.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+ 
+    var oneGmach = new GMach.Model.OneGmach();
+   
+    $("#inputMoney").text(oneGmach.GetInputMoney());
+    $("#outputMoney").text(oneGmach.GetOutputMoney());
+    $("#diffMoney").text(oneGmach.GetDiffMoney());
+});
