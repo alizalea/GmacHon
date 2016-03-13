1b53b142eda705f773e6e53b79c85a7f9d694cfe
alizalea <lea.tvito@gmail.com>
2016-03-14 00:59:46 +0200

    איש קשר

--- JS/DAL/ContactDAL.js
+++ JS/DAL/ContactDAL.js
@@ -1,15 +1,60 @@
-﻿var GMach = GMach || {};
+﻿var Contacts = {};
+var GMach = GMach || {};
 GMach.DAL = GMach.DAL || {};
 GMach.DAL.Contact = GMach.DAL.Contact || {};
 
-GMach.DAL.Contact.GetAllContacts = function () {
-    var Contacts = [new GMach.Model.Contact("משה", "כהן", "111111111", "02-1111111", "054-8402125", "", "תורם חבל על הזמן"),
-                                 new GMach.Model.Contact("יוסי", "לוי", "222222222", "024444444", "085-9999999", "", "חיחחייח"),
-                                 new GMach.Model.Contact("יוסי", "כהן", "33333333", "024444444", "085-4444444", "", "חיחחייח"),
-                                 new GMach.Model.Contact("יוסי", "לוי", "44444444", "024444444", "085-5555555", "", "חיחחייח"),
-                                 new GMach.Model.Contact("חיים", "טויו", "222222222", "024444444", "085-9999999", "", "חיחחייח"),
-                                 new GMach.Model.Contact("אריאל", "פכטר", "222222222", "024444444", "085-9999999", "", "חיחחייח")];
+   
 
+
+GMach.DAL.Contact.GetAllContacts = function () {
+    Contacts = [new GMach.Model.Contact("משה", "כהן", "111111111", "02-1111111", "054-8402125", "", "תורם חבל על הזמן"),
+                                new GMach.Model.Contact("יוסי", "לוי", "222222222", "024444444", "085-9999999", "", "חיחחייח"),
+                                new GMach.Model.Contact("יוסי", "כהן", "33333333", "024444444", "085-4444444", "", "חיחחייח"),
+                                new GMach.Model.Contact("יוסי", "לוי", "44444444", "024444444", "085-5555555", "", "חיחחייח"),
+                                new GMach.Model.Contact("חיים", "טויו", "222222222", "024444444", "085-9999999", "", "חיחחייח"),
+                                new GMach.Model.Contact("אריאל", "פכטר", "222222222", "024444444", "085-9999999", "", "חיחחייח")];
     return Contacts;
+
+}
+
+GMach.DAL.Contact.GetDatataContact = function (id) {
+    var result = $.grep(GMach.DAL.Contact.GetAllContacts(), function (e) { return e.id == id; });
+    if (result.length == 0) {
+        // not found
+        return new GMach.Model.Contact();
+    } else if (result.length == 1) {
+        return result[0];
+    }
+    return null
+    //else {
+        // multiple items found
+   // }
+    
+
+   
+
+}
+GMach.DAL.Contact.SetDatataContact = function (contact) {
+    try {
+        var c = null;
+        if (contact.id == undefined) {
+             c = new GMach.Model.Contact();
+        } else {
+            c = GMach.DAL.Contact.GetDatataContact(contact.id)
+        }
+            c.firstName = contact.firstName;
+            c.lastName = contact.lastName;
+            c.IdNumber = contact.IdNumber;
+            c.phoneNumber = contact.phoneNumber;
+            c.mobileNumber = contact.mobileNumber;
+            c.address = contact.address;
+            c.remarks = contact.remarks;
+
+            Contacts.push(c);
+            return true;
+        
+    } catch (e) {
+        return false;
+    }
 
 }
--- JS/Model/ContactModel.js
+++ JS/Model/ContactModel.js
@@ -1,9 +1,17 @@
-﻿var GMach = GMach || {};
+﻿
+var GMach = GMach || {};
 GMach.Model = GMach.Model || {};
 GMach.Model.Contact = GMach.Model.Contact || {};
 
 
 GMach.Model.Contact = function (firstName, lastName, IdNumber, phoneNumber, mobileNumber, address, remarks) {
+    if (GMach.Model.Contact.indexid == undefined) {
+        GMach.Model.Contact.indexid = 1;
+    }
+    else {
+        GMach.Model.Contact.indexid++;
+    }
+    this.id = GMach.Model.Contact.indexid;
     this.firstName = firstName;
     this.lastName = lastName;
     this.IdNumber = IdNumber;
@@ -16,6 +24,16 @@ GMach.Model.Contact = function (firstName, lastName, IdNumber, phoneNumber, mobi
 GMach.Model.Contact.GetAllContacts = function () {
    
     return GMach.DAL.Contact.GetAllContacts();
+
+}
+GMach.Model.Contact.GetDatataContact = function (id) {
+
+    return GMach.DAL.Contact.GetDatataContact(id);
+
+}
+GMach.Model.Contact.SetDatataContact = function (contact) {
+
+    return GMach.DAL.Contact.SetDatataContact(contact);
 
 }
 
