6330f0f6b73e4a2077d2a993ac0ea8a64e3adea4
lea <lea.tvito@gmail.com>
2016-03-18 01:42:01 +0200

    aaa

--- JS/UI/OneGmachUI.js
+++ JS/UI/OneGmachUI.js
@@ -1,12 +1,15 @@
 ﻿$(document).ready(function () {
  
-    var oneGmach = new GMach.Model.OneGmachObject();
-   
+    var oneGmach = new GMach.Model.OneGmach();
+
     $("#inputMoney").text(numberWithCommas(oneGmach.inputMoney));
     $("#outputMoney").text(numberWithCommas(oneGmach.outputMoney));
     $("#diffMoney").text(numberWithCommas(oneGmach.diffMoney));
 });
 
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    if (x != undefined) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    else throw "Undefined Number"
 }
