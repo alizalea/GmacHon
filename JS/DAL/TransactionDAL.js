65e31c0d31c7f3241ad27344ba18e63e1bd4d1a2
lea <lea.tvito@gmail.com>
2016-03-28 22:37:36 +0300

    Transaction

--- JS/DAL/TransactionDAL.js
+++ JS/DAL/TransactionDAL.js
@@ -3,13 +3,7 @@ GMach.DAL = GMach.DAL || {}; // global namespace, all good
 GMach.DAL.Transaction = GMach.DAL.Transaction || {}; // global namespace, all good
 
 GMach.DAL.Transaction.GetAllTransactions = function () {
-    //var Transactions = [new GMach.Model.Transaction.Loan(1, 1, -5, "2012-02-01", new GMach.Model.Transaction.Freind("aliza", "twito", "0548402125", "test"), new GMach.Model.Transaction.Freind("fff", "hhhh", "0548402888", "test2"), null, false, 100),
-    //                    new GMach.Model.Transaction.ReturnLoan(2, 2, 20, "2012-03-04"),
-    //                    new GMach.Model.Transaction.Deposit(3, 3, 60, "2012-02-22", "2012-02-25", true, 20)
 
-    //];
-
-    //return Transactions;
     var transactionData = [
   {
       "id": 1,
