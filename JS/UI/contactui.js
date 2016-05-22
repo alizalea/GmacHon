85b0b1ab4cee561d31cb00cdf14aba4d668d9aee
lea <lea.tvito@gmail.com>
2016-05-22 22:58:03 +0300

    contact

--- JS/UI/contactui.js
+++ JS/UI/contactui.js
@@ -21,114 +21,120 @@ var contact = null;
 
 
 $(document).ready(function () {
+    if ($("#contacts").length) {
+        AllContactsOnLoad();
+    }
     if ($("#Contact-form").length) {
+        ContactOnLoad();
+    }
+});
 
-        if (idfromqs != null) {
-            contact = GMach.Model.Contact.GetDataContact(idfromqs);
-            $('#FirstName').val(contact.firstName);
-            $('#LastName').val(contact.lastName);
-            $('#IdNumber').val(contact.IdNumber);
-            $('#PhoneNumber').val(contact.phoneNumber);
-            $('#MobileNumber').val(contact.mobileNumber);
-            $('#Address').val(contact.address);
-            $('#Remarks').val(contact.remarks);
-            //$('h2').text(f+" " +l);
-            LoadLoanContact(idfromqs);
-        } else {
-            $('#TransactionContact').hide();
-            contact = new GMach.Model.Contact();
-            //var oneGmach = new GMach.Model.OneGmach();
-            //contact.id = oneGmach.nextContactID;
+function AllContactsOnLoad()
+{
+    $('#contacts').DataTable({
+        //  dom: "Bfrtip",
+
+        //ajax: "../php/todo.php",
+        data: allContacts,
+
+        columns: [
+             { "data": "lastName" },
+             { "data": "firstName" },
+             { "data": "IdNumber" },
+             { "data": "phoneNumber" },
+             { "data": "mobileNumber" },
+             { "data": "remarks" }
+        ],
+        select: true,
+
+        /*  buttons: [
+  
+             { extend: "create", editor: editor },
+  
+              { extend: "edit", editor: editor },
+  
+              { extend: "remove", editor: editor }
+  
+          ],*/
+        "language": {
+
+            "lengthMenu": "מציג _MENU_ שורות לעמוד",
+
+            "zeroRecords": "לא נמצאו אנשי קשר",
+
+            "info": "מספר דף _PAGE_ מתוך _PAGES_",
+
+            "infoEmpty": "אין פריטים לתצוגה",
+
+            "infoFiltered": "(filtered from _MAX_ total records)",
+            "search": "חיפוש",
+
+            "paginate": {
+
+                "first": "עמוד ראשון",
+
+                "last": "עמוד אחרון",
+
+                "next": "הבא",
+
+                "previous": "הקודם"
+
+            },
+        }
+    });
+
+    var table = $('#contacts').DataTable();
+
+    $('#contacts tbody').on('click', 'tr', function () {
+        var id = table.row(this).data().id;
+        window.location = "/HTML/Contact.html?id=" + id;
+
+    });
+}
+
+function ContactOnLoad()
+{
+    if (idfromqs != null) {
+        contact = GMach.Model.Contact.GetDataContact(idfromqs);
+        $('#FirstName').val(contact.firstName);
+        $('#LastName').val(contact.lastName);
+        $('#IdNumber').val(contact.IdNumber);
+        $('#PhoneNumber').val(contact.phoneNumber);
+        $('#MobileNumber').val(contact.mobileNumber);
+        $('#Address').val(contact.address);
+        $('#Remarks').val(contact.remarks);
+        //$('h2').text(f+" " +l);
+        LoadLoanContact(idfromqs);
+    } else {
+        $('#TransactionContact').hide();
+        contact = new GMach.Model.Contact();
+        //var oneGmach = new GMach.Model.OneGmach();
+        //contact.id = oneGmach.nextContactID;
+    }
+
+    $("#btn_save").click(function () {
+        // if (isvalid()) {
+
+        // }
+        contact.firstName = $('#FirstName').val();
+        contact.lastName = $('#LastName').val();
+        contact.IdNumber = $('#IdNumber').val();
+        contact.phoneNumber = $('#PhoneNumber').val();
+        contact.mobileNumber = $('#MobileNumber').val();
+        contact.address = $('#Address').val();
+        contact.remarks = $('#Remarks').val();
+        debugger;
+        if (GMach.Model.Contact.SetDataContact(contact, idfromqs)) {
+            window.location = "/HTML/Contacts.html";
         }
 
-        $("#btn_save").click(function () {
-            // if (isvalid()) {
 
-            // }
-            contact.firstName = $('#FirstName').val();
-            contact.lastName = $('#LastName').val();
-            contact.IdNumber = $('#IdNumber').val();
-            contact.phoneNumber = $('#PhoneNumber').val();
-            contact.mobileNumber = $('#MobileNumber').val();
-            contact.address = $('#Address').val();
-            contact.remarks = $('#Remarks').val();
-            debugger;
-            if (GMach.Model.Contact.SetDataContact(contact, idfromqs)) {
-                window.location = "/HTML/Contacts.html";
-            }
+    });
+    $("#btn_cancel").click(function (contact) {
+        window.location = "/HTML/Contacts.html";
 
-
-        });
-        $("#btn_cancel").click(function (contact) {
-            window.location = "/HTML/Contacts.html";
-
-        });
-    }
-    if ($("#contacts").length) {
-
-        $('#contacts').DataTable({
-            //  dom: "Bfrtip",
-
-            //ajax: "../php/todo.php",
-            data: allContacts,
-
-            columns: [
-                 { "data": "lastName" },
-                 { "data": "firstName" },
-                 { "data": "IdNumber" },
-                 { "data": "phoneNumber" },
-                 { "data": "mobileNumber" },
-                 { "data": "remarks" }
-            ],
-            select: true,
-
-            /*  buttons: [
-      
-                 { extend: "create", editor: editor },
-      
-                  { extend: "edit", editor: editor },
-      
-                  { extend: "remove", editor: editor }
-      
-              ],*/
-            "language": {
-
-                "lengthMenu": "מציג _MENU_ שורות לעמוד",
-
-                "zeroRecords": "לא נמצאו אנשי קשר",
-
-                "info": "מספר דף _PAGE_ מתוך _PAGES_",
-
-                "infoEmpty": "אין פריטים לתצוגה",
-
-                "infoFiltered": "(filtered from _MAX_ total records)",
-                "search": "חיפוש",
-
-                "paginate": {
-
-                    "first": "עמוד ראשון",
-
-                    "last": "עמוד אחרון",
-
-                    "next": "הבא",
-
-                    "previous": "הקודם"
-
-                },
-            }
-        });
-
-        var table = $('#contacts').DataTable();
-
-        $('#contacts tbody').on('click', 'tr', function () {
-            var id = table.row(this).data().id;
-            window.location = "/HTML/Contact.html?id=" + id;
-
-        });
-
-    }
-
-});
+    });
+}
 
 function LoadLoanContact(id) {
     var contactTransactions = GMach.Model.Contact.GetContactTransactions(id);
