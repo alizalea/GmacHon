da70bccea5b73e799b4c06f032847420c163d0b8
alizalea <lea.tvito@gmail.com>
2016-06-28 23:19:40 +0300

    k

--- JS/UI/OneGmachUI.js
+++ JS/UI/OneGmachUI.js
@@ -19,9 +19,9 @@ function getMasseges() {
     var returnTransactions = GMach.Model.Transaction.GetReturnDepositTransactions();
 
     returnTransactions.forEach(function (tran) {
-        var contact = GMach.Model.Contact.GetDataContact(tran.contact);
+        var contact = GMach.Model.Contact.GetDataContact(tran.contactId);
         var contactName = contact.firstName + " " + contact.lastName;
-        massages += "בתאריך " + tran.returnDate + " עליך להחזיר סכום של " + tran.amount + " ₪ ל" + contactName;
+        massages += "בתאריך " + new Date(tran.returnDate).toJSON().slice(0, 10) + " עליך להחזיר סכום של " + tran.amount + " ₪ ל" + contactName;
         massages += "\n";
     }
      );
