3855b68e4fd131af850c8e34acb44d9263096738
lea <lea.tvito@gmail.com>
2016-08-07 19:59:27 +0300

    add video buttons

---
+++ JS/UI/VideoUI.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    console.error("gg");
+    var media = $('#video')[0];
+    $('#playVideo').click(function () {
+        console.error("ff");
+        if (!media.paused) {
+            media.pause();
+            this.innerText = "הפעל";
+            media.currentTime = 0;
+        } else {
+            media.play();
+            this.innerText = "עצור";
+        }
+    });
+});
